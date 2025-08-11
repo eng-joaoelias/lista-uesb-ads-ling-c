81d05bbf21a41535eed97b4c0c11e0a849baba15
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2025-08-11 15:20:58 -0300

    Create q05.c

---
+++ IntroducaoAVariaveis/q05.c
@@ -0,0 +1,19 @@
+/*
+Aluno: Joao Elias Ferraz Santana
+Curso: Analise e Desenvolvimento de Sistemas
+Universidade Estadual do Sudoeste da Bahia
+
+5 - Elabore um programa que escreva as mensagens “Início do programa” e “Fim”
+na tela, uma em cada linha, usando apenas um comando printf.
+*/
+
+#include <stdio.h>
+
+int main(){
+    
+    printf("Início do programa\nFim\n");
+    printf("Pressione <enter> para sair...");
+    getchar();
+
+    return 0;
+}
